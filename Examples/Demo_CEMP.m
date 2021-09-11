7b09bd889f16939bdbeb5dd8df7ac9a8586be097
Yunpeng Shi <yunpengs@princeton.edu>
2021-09-11 10:53:08 -0400

    Add files via upload

---
+++ Examples/Demo_CEMP.m
@@ -0,0 +1,21 @@
+
+model_out = Uniform_Topology(200,0.5,0.3,0,'uniform');
+
+Ind = model_out.Ind; % matrix of edge indices (m by 2)
+RijMat = model_out.RijMat; % given corrupted and noisy relative rotations
+ErrVec = model_out.ErrVec; % ground truth corruption levels
+R_orig = model_out.R_orig; % ground truth rotations
+
+% set CEMP defult parameters
+CEMP_parameters.max_iter = 6;
+CEMP_parameters.reweighting = 2.^((1:6)-1);
+CEMP_parameters.nsample = 50;
+
+% run CEMP
+SVec = CEMP(Ind,RijMat,CEMP_parameters);
+
+%visualize sij^* and sij,t, ideally one should see a straight line "y=x"
+plot(ErrVec,SVec,'b.');
+title('Scatter Plot of s_{ij}^* v.s. s_{ij,T}')
+xlabel('s_{ij}^*') 
+ylabel('s_{ij,T}') 
